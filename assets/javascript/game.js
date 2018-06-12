c9164fcf5403342f5e04c5b533a886c07fa32ab4
mpeng <michaelxpeng@gmail.com>
2018-06-11 23:57:32 -0400

    pseudocodes

---
+++ assets/javascript/game.js
@@ -0,0 +1,32 @@
+// Create global variables for
+    // Randomly generated target number
+    // Win counter
+    // Loss counter
+    // Random number for crystal 1
+    // Random number for crystal 2
+    // Random number for crystal 3
+    // Random number for crystal 4
+    // User's tally
+
+
+// Initialize the game
+    // Set win counter, loss counter and user's tally to 0
+
+// Start the game
+    // Ask the computer to randomly generate a target number between 19-120 at the start of each game
+    // Print that number to the box for the generated target number
+    // Ask the computer to randomly generate a number between 1-12 for each of the crystals
+    // Print the assigned number to user's tally upon each click
+    // Allow user's tally to update the numbers according to the crystals clicked
+
+// Check to see if the user's tally matches the generated target number
+    // If it matches
+        // Win counter increases
+        // Game resets
+        // Computer randomly generates another target number
+        // Computer randomly generates another set of numbers for the crystals
+    // If it exceeds the target number
+        // Loss counter increases
+        // Game resets
+        // Computer randomly generates another target number
+        // Computer randomly generates another set of numbers for the crystals
